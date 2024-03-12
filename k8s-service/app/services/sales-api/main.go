eb4de70c7507f03a5f3d3fa353ed3821ec92c8b2
baojingh@163.com <baojingh@163.com>
2024-03-12 17:54:26 +0800

    update

--- k8s-service/app/services/sales-api/main.go
+++ k8s-service/app/services/sales-api/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 
 	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var build = "develop"
 
 func main() {
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+	if err := run(log); err != nil {
+		log.ErrorW("startup", "ERROR", err)
+		os.Exit(1)
+	}
 
 	if _, err := maxprocs.Set(); err != nil {
 		fmt.Errorf("maxprocs %w", err)
@@ -29,5 +40,25 @@ func main() {
 	<-shutdown
 
 	log.Println("service is ending")
+
+}
+
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
+
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+	return log.Sugar(), nil
 
 }
--- k8s-service/go.mod
+++ k8s-service/go.mod
@@ -2,4 +2,9 @@ module k8s-service
 
 go 1.19
 
-require go.uber.org/automaxprocs v1.5.3
+require (
+	go.uber.org/automaxprocs v1.5.3
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
